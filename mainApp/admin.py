dbd3f30edfa48bb0d8a5cecac522f768da8f3c45
erwing23 <erwingdelfines@hotmail.com>
2022-08-01 20:36:41 -0500

    relations

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Caso, Organizacion
 # Register your models here.
+
+admin.site.register(Caso)
+admin.site.register(Organizacion)
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Caso(models.Model):
+    dueñoCaso = models.ForeignKey(User, on_delete=models.CASCADE)  # new
+    organizacion = models.CharField(max_length = 254)
+    estado = models.BooleanField(default=False)
+    idCaso = models.CharField(max_length = 254)
+    tramite = models.CharField(max_length = 254)
+
+class Organizacion(models.Model):
+    organizacionUser = models.ForeignKey(User, on_delete=models.CASCADE)  # new
+    nombre = models.CharField(max_length = 254)
+    direccion = models.CharField(max_length = 254)
+    contacto = models.CharField(max_length = 254)
+    email = models.EmailField(max_length = 254)
+
