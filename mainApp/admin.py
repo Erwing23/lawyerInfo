220d3529f4dec38947f53f9d0e817eee2137de4e
erwing23 <erwingdelfines@hotmail.com>
2022-08-03 18:12:55 -0500

    static

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -3,15 +3,25 @@ from .models import Caso, Organizacion, PowerUp, Aprobaciones, Contacto
 # Register your models here.
 
 class CasoAdmin(admin.ModelAdmin):
-    list_display = ("id","dueñoCaso","organizacionName","estado","idCaso","tramite","fecha_inicio","fecha_fin" )
+#    list_display = ("id","dueñoCaso","organizacionName","estado","idCaso","tramite","fecha_inicio","fecha_fin" )
+     list_display = ("id", "organizacionName")
+
 admin.site.register(Caso, CasoAdmin)
 
 class OrganizacionAdmin(admin.ModelAdmin):
-    list_display = ("id","organizacionUser","nombre","direccion","contacto","email")
+#    list_display = ("id","organizacionUser","nombre","direccion","contacto","email")
+     list_display = ("id",)
+
 admin.site.register(Organizacion, OrganizacionAdmin)
-admin.site.register(Aprobaciones)
+
+class AprobacionesAdmin(admin.ModelAdmin):
+#    list_display = ("id","casoEncargado","sociosEncargados","gerenciaFinanciera","pagaduria")
+    list_display = ("id", )
+
+admin.site.register(Aprobaciones,AprobacionesAdmin)
 admin.site.register(Contacto)
 class PowerUpAdmin(admin.ModelAdmin):
-    list_display = ("User","id","estadoWhatsapp","estadoSMS")
+   # list_display = ("User","id","estadoWhatsapp","estadoSMS")
+    list_display = ("id","name")
 
 admin.site.register(PowerUp,PowerUpAdmin)
--- mainApp/models.py
+++ mainApp/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# MAÑANA VISUAL FORMS TABLAS
+# JUEVES REQUEST WHATSAPP - ARCHIVOS -
+# VIENRES Mailing - ARCHIVOS
+# SABADO DOMINGO INTERFAZ
 
 
 class Organizacion(models.Model):
     organizacionUser = models.ForeignKey(User, on_delete=models.CASCADE)  # new
     nombre = models.CharField(max_length = 254)
     direccion = models.CharField(max_length = 254)
-    contacto = models.CharField(max_length = 254)
+    contacto_organizacion = models.CharField(max_length = 254) # PROPIO CONTACTO
     email = models.EmailField(max_length = 254)
     def __str__(self):
         return self.nombre
@@ -18,34 +22,49 @@ class Caso(models.Model):
     organizacionName = models.ForeignKey(Organizacion, on_delete=models.CASCADE, null=True) 
     estado = models.BooleanField(default=False)
     idCaso = models.CharField(max_length = 254)
-    tramite = models.CharField(max_length = 254)
-    fecha_inicio = models.DateField(blank=True)
-    fecha_fin = models.DateField(blank=True)
+    tramite = models.CharField(max_length = 254) # PDF 
+    fecha_inicio = models.DateField(auto_now_add=True)# CUANDO SE NOW()
+    fecha_fin = models.DateField(blank=True) # BLANCO HASTA CAMBIAR DE ESTADO CERRADO
     def __str__(self):
-        return self.tramite
+        return self.tramite + self.dueñoCaso.username
     
 class Contacto(models.Model):
     User = models.ForeignKey(User, on_delete=models.CASCADE)  # new
+    organizacionName = models.ForeignKey(Organizacion, on_delete=models.CASCADE,  null=True)  # new
     nombre = models.CharField(max_length = 254)
     apellido = models.CharField(max_length = 254)
     celular = models.CharField(max_length = 254)
+    departamento = models.CharField(max_length = 254, null=True)
     email = models.EmailField(max_length = 254)
     def __str__(self):
-        return self.nombre
+        return self.nombre + " " +self.organizacionName.nombre
 class PowerUp(models.Model):
     User = models.ForeignKey(User, on_delete=models.CASCADE)  # new
-    estadoWhatsapp = models.BooleanField(default=False)
-    estadoSMS = models.BooleanField(default=False)
+    name = models.CharField(max_length = 254, default="vacio") # Whasap SMS
+    url = models.CharField(max_length = 254, default="www.url.com")
+    port = models.CharField(max_length = 254, default="8080")
+    tipo = models.CharField(max_length = 254, default="get")
+   
     def __str__(self):
         return self.User.username
 
-class Aprobaciones(models.Model):
-    casoEncargado = models.ForeignKey(Caso, on_delete=models.CASCADE)  # new
-    sociosEncargados = models.BooleanField(default=False)
-    gerenciaFinanciera = models.BooleanField(default=False)
-    pagaduria = models.BooleanField(default=False)
+class Aprobaciones(models.Model): #CONTACTO - ESTADO
+    class Meta:
+        verbose_name_plural = "Aprobaciones"
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    contactoQueAprueba = models.ForeignKey(Contacto, on_delete=models.CASCADE)  # new
     def __str__(self):
-        return self.casoEncargado.dueñoCaso.username
+        return self.User.username
+class Estado(models.Model):
+    class Status(models.TextChoices):
+        ACTIVO = "1", "ACTIVO"
+        INACTIVO = "2", "INACTIVO"
+        ABIERTO = "3", "ABIERTO"
+        CERRADO = "4", "CERRADO"
+
+    def __str__(self):
+        return self.User.username
+
 
 
 
