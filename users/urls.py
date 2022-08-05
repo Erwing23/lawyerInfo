c0e4f9c08d597d208ec3bd193be0dd236ec3625c
erwing23 <erwingdelfines@hotmail.com>
2022-08-04 21:32:12 -0500

    cambios

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
 
  path('', views.home, name = "home"),
- path('salir/', views.salir, name="salir")
+ path('salir/', views.salir, name="salir"),
+ path('tableCaso/', views.tableCaso, name="tableCaso"),
+ path('powerUp/', views.powerUp, name="powerUp"),
+
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout 
+from mainApp.models import PowerUp
+import requests
 # Create your views here.
 @login_required
 def home(request):
@@ -8,6 +10,18 @@ def home(request):
     return render(request,"users/index.html")
 
 def salir(request):
-    print(request.user.get_full_name())
+    print(request.user.username)
     logout(request)
-    return redirect("/home")
+    return redirect("/")
+
+@login_required
+def tableCaso(request):
+    return render(request,"users/tables.html")
+
+
+@login_required
+def powerUp(request):
+    print("pilas")
+    print(request.user.username)
+   # return render(request,"users/tables.html")
+
